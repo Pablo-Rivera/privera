895038b3b5c5fe9c2b2b1cdc66362b7a2a2d18fc
Pablo rivera <paliyo@gmail.com>
2015-06-20 11:00:18 -0300

    sumando ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+	$("#sabores").on("click",function(event){
+		event.preventDefault();
+		$.ajax({
+			type: "GET",
+			dataType: "html",
+			url: "homex.html",
+			success: function(data){
+				$("#cuerpo").html(data);
+			},
+			error: function(){
+				alert("error");
+			}
+		})
+	})
+});
+$(document).ready(function(){
 	$("#Home").on("click",function(event){
 		event.preventDefault();
 		$.ajax({
