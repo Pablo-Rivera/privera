f760365698745df150307273f3d3e2561a8fa059
Pablo Rivera <paliyo@gmail.com>
2015-06-23 23:50:52 -0300

    100% ajax y dom

--- js/ajax.js
+++ js/ajax.js
@@ -62,12 +62,14 @@ $(document).ready(function(){
 		})
 	})
 });
+$('document').ready(function(){
 
-	function guardarInformacion(){
+	// Definicion de Variables
+	grupo = 22;
+	function guardarInformacion(grupo){
 			var porciones = $("#porciones").val();
 		  var cantidad = $("#cantidad").val();
 		  var precio = $("#precio").val();
-		  var grupo = 22;
 		  var informacion = [porciones, cantidad, precio];
 		  var info = {
 		      "group": grupo,
@@ -82,7 +84,7 @@ $(document).ready(function(){
 		       contentType: "application/json; charset=utf-8",
 		       url: "http://web-unicen.herokuapp.com/api/create",
 					success: function(data){
-							 traerTabla();
+							 traerTabla(grupo);
 			         alert('Deploy Success');
 			       },
 			       error:function(data){
@@ -92,24 +94,32 @@ $(document).ready(function(){
 		  }
 	}
 
-function traerTabla(){
-  var grupo = 22;
-  $.ajax({
-     type: "GET",
-     dataType: 'JSON',
-     url: "http://web-unicen.herokuapp.com/api/group/" + grupo,
-     success: function(data){
-        var porciones = "";
-        var cantidad = "";
-        var precio = "";
-				$("#cuerpotabla").html('');
-        for (var i = 1; i < data.information.length; i++) {
-	         porciones = data.information[i]['thing'][0];
-	         cantidad = data.information[i]['thing'][1];
-	         precio = data.information[i]['thing'][2];
-	         $("#cuerpotabla").append("<tr><td>" + porciones + "</td><td>" + cantidad + "</td><td>" + precio + "</td></tr>");
-        }
+	function traerTabla(grupo){
+	  $.ajax({
+	     type: "GET",
+	     dataType: 'JSON',
+	     url: "http://web-unicen.herokuapp.com/api/group/" + grupo,
+	     success: function(data){
+	        var porciones = "";
+	        var cantidad = "";
+	        var precio = "";
+					$("#cuerpotabla").html('');
+	        for (var i = 1; i < data.information.length; i++) {
+		         porciones = data.information[i]['thing'][0];
+		         cantidad = data.information[i]['thing'][1];
+		         precio = data.information[i]['thing'][2];
+		         $("#cuerpotabla").append("<tr><td>" + porciones + "</td><td>" + cantidad + "</td><td>" + precio + "</td></tr>");
+	        }
 
-     }
-  });
-}
+	     }
+	  });
+	}
+
+		traerTabla(grupo);
+
+		// Llama a la Funcion cargarActividades cuando se Presiona el Boton Correspondiente
+		$('#agregar').on('click', function(event){
+			event.preventDefault();
+			guardarInformacion(grupo);
+		});
+	});
